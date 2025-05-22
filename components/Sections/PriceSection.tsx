6e2939b5d07b90b6902a9e7d98c56b29990a621a
OfficeB <office@ih-club.com>
2025-05-22 11:31:03 +0200

    Remove unused Button import from PricingSection component

--- components/Sections/PriceSection.tsx
+++ components/Sections/PriceSection.tsx
@@ -1,5 +1,4 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import { BadgeDollarSign, Gift, Users } from "lucide-react";
 
 export default function PricingSection() {
