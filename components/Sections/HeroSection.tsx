60811e466e37fa8de4d643d8db6d2e4a45c0dbec
OfficeB <office@ih-club.com>
2025-05-22 11:04:57 +0200

    Increase font size of the main heading and adjust paragraph size in HeroSection for better readability

--- components/Sections/HeroSection.tsx
+++ components/Sections/HeroSection.tsx
@@ -7,13 +7,13 @@ export default function HeroSection() {
         <section id='hero' className=" py-10 px-6  w-full flex flex-col-reverse md:flex-row items-center justify-end gap-4 max-w-7xl mx-auto md:min-h-screen">
             {/* Left side */}
             <div className="w-full  text-center md:text-left space-y-4">
-                <h1 className="text-4xl sm:text-6xl font-extrabold text-gray-900 leading-tight">
+                <h1 className="text-5xl sm:text-6xl font-extrabold text-gray-900 leading-tight">
                     Tvoj <span className="text-orange-600">Bedž.</span> Tvoja Priča.
                 </h1>
-                <p className="text-gray-700 text-xl max-w-md mx-auto md:mx-0">
+                <p className="text-gray-700 text-lg max-w-md mx-auto md:mx-0">
                     Odaberi boju, ubaci svoju sliku ili tekst, i kreiraj bedž koji je baš po tvojoj meri. Ako nemaš inspiraciju, tu su i naši predlozi!
                 </p>
-                <div className="flex flex-col sm:flex-row justify-center md:justify-start gap-3 w-full sm:w-auto pt-4">
+                <div className="flex flex-col sm:flex-row justify-center md:justify-start gap-3 w-full sm:w-auto pt-2">
                     <Link href="#bedzevi" className="w-full sm:w-auto">
                         <Button className="text-lg font-semibold px-6 py-6 w-full sm:w-auto bg-orange-600 text-white hover:bg-orange-700 transition duration-200">
                             Dizajniraj svoj bedž
