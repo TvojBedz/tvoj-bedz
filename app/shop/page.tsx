be6fe8d7c676ffe9c0c3721f7a57b580a9883526
OfficeB <office@ih-club.com>
2025-05-22 15:15:11 +0200

    Refactor ShopPage component to accept a more flexible structure for searchParams and improve parameter handling

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,13 +1,14 @@
 import Shop from "@/components/Shop/Shop";
 
-const ShopPage = async (props: { searchParams: Promise<URLSearchParams> }) => {
-    const searchParams = await props.searchParams;
-    const page = parseInt(searchParams.get('page') || "1");
-
+const ShopPage = async ({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) => {
+    const params = await searchParams;
+    const page = parseInt(params.page as string) || 1;
 
     return (
         <Shop page={page} />
     )
 }
+
+
 
 export default ShopPage
