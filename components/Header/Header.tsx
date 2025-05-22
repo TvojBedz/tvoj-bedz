d62f646fdd75cf32fc56c53db186c506d82b490d
OfficeB <office@ih-club.com>
2025-05-22 11:08:45 +0200

    Refactor Header component for improved styling and transition effects

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -41,7 +41,7 @@ export default function Header() {
 
 
     return (
-        <header className="w-full border-b bg-white/60 backdrop-blur-md shadow-md sticky top-0 z-50 rounded-b-xl">
+        <header className={`w-full border-b bg-white shadow-sm sticky top-0 z-50 transition-all duration-300 ${!open ? 'backdrop-blur-md bg-white/60' : ''}`}>
             <div className="container mx-auto flex items-center justify-between p-4 px-6 md:px-20">
                 {/* Logo */}
                 <Link href="/#hero" scroll={false} onClick={(e) => openLink(e, 'hero')} className="text-2xl font-bold text-black">
